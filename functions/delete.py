d5c49bfea90d589d1a2ab4959519cb80c2e8ae7f
akramfd12 <umarofahrul@gmail.com>
2025-11-11 21:27:53 +0700

    feat: add delete functionality for borrowing records

--- functions/delete.py
+++ functions/delete.py
@@ -1,5 +1,9 @@
-from .db_config import delete_data_book
+from .db_config import delete_data_book, delete_data_pinjam
 def delete_book(input_pilih_delete):
     # input_pilih_delete = input("Masukkan Judul Buku: ")
     delete_data_book(input_pilih_delete)
     print("Data buku berhasil dihapus!")
+
+def delete_pinjam(input_pilih_delete):
+    delete_data_pinjam(input_pilih_delete)
+    print("Data peminjam berhasil dihapus")
