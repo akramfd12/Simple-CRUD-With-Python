9fa870a1b7c72e7353c3af63f9a098bb52cc7e16
akramfd12 <umarofahrul@gmail.com>
2025-11-11 21:27:32 +0700

    feat: enhance admin functionality with improved book and borrowing management

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ import sys
 import pandas as pd
 from functions.create import create_book, create_pinjam
 from functions.update import update_book, update_flagpinjam
-from functions.read import read_book, search_book, read_pinjam
-from functions.delete import delete_book
+from functions.read import read_book, search_book, read_pinjam, search_pinjam, merge_book_pinjam
+from functions.delete import delete_book, delete_pinjam
 from functions.exitprog import close_prog
 from functions.menu import * 
 
 def test():
-    df = read_pinjam()
+    pd.set_option('display.max_columns', None)
+    df = merge_book_pinjam()
     print(df)
-    input_user = (input("Masukkan ID: "))
-    update_flagpinjam(input_user)
+    # input_user = int((input("Masukkan ID: ")))
+    # df_search_pinjam = search_pinjam(input_user)
+    # print(df_search_pinjam)
 
 def admin():
     pd.set_option('display.max_columns', None)
@@ -26,18 +28,49 @@ def admin():
             
         #Read Data
         if input_user == 2:
-            df = read_book()
-            print(df)
-            print(menu_search())
             while True:
-                input_search_book = int(input("\nMasukkan angka: "))
-                if input_search_book == 1:
-                    input_user_read = int(input("\nMasukkan ID buku: "))
-                    df_search = search_book(input_user_read)
-                    print(df_search)
-                    print(menu_search())
-                elif input_search_book == 2:
-                    # keluar dari menu search → kembali ke menu admin
+                print(menu_admin_read())
+                input_menu_read = int(input("\nMasukkan angka: ")) 
+                #Menampilkan Data Buku
+                if input_menu_read == 1:
+                    df = read_book()
+                    df_total_buku = len(df)
+                    print(df)
+                    print(f"Total Buku {df_total_buku}")
+                    print(menu_search_book())
+                    while True:
+                        input_search_book = int(input("\nMasukkan angka: "))
+                        #Search Book
+                        if input_search_book == 1:
+                            input_user_read = int(input("\nMasukkan ID buku: "))
+                            df_search_book = search_book(input_user_read)
+                            print(df_search_book)
+                            print(menu_search_book())
+                        elif input_search_book == 2:
+                            # keluar dari menu search → kembali ke menu admin
+                            break
+                        else:
+                            print("Pilihan tidak valid!")
+                #Menampilkan Data Peminjaman
+                elif input_menu_read == 2:
+                    df_pinjam = read_pinjam()
+                    df_total_buku = len(df_pinjam)
+                    print(df_pinjam)
+                    print(f"Total peminjam {df_total_buku}")
+                    print(menu_search_pinjam())
+                    while True :
+                        input_search_pinjam = int(input("\nMasukkan angka: "))
+                        #Search Peminjam
+                        if input_search_pinjam == 1:
+                            input_user_pinjam = int(input("\nMasukkan ID peminjam: "))
+                            df_search_pinjam = search_pinjam(input_user_pinjam)
+                            print(df_search_pinjam)
+                        elif input_search_pinjam == 2:
+                            # keluar dari menu search → kembali ke menu admin
+                            break
+                        else:
+                            print("Pilihan tidak valid!")
+                elif input_menu_read == 3:
                     break
                 else:
                     print("Pilihan tidak valid!")
@@ -51,10 +84,32 @@ def admin():
 
         #Delete Data
         if input_user == 4:
-            df = read_book()
-            print(df)
-            input_pilih_delete = input("\nMasukkan ID yang mau dihapus: ")
-            delete_book(input_pilih_delete)
+            print(menu_delete())
+            while True:
+                print(menu_delete())
+                input_delete = int(input("\nMasukkan angka: "))
+                if input_delete == 1:
+                    df = read_book()
+                    print(df)
+                    input_pilih_delete_book = int(input("\nMasukkan ID yang mau dihapus: "))
+                    delete_book(input_pilih_delete_book)
+                elif input_delete == 2:
+                    df_pinjam = read_pinjam()
+                    print(df_pinjam)
+                    input_pilih_delete_pinjam = int(input("\nMasukkan ID yang mau dihapus: "))
+                    delete_pinjam(input_pilih_delete_pinjam)
+                elif input_delete == 3:
+                    break
+                else:
+                    print("Pilihan tidak valid!")
+
+        #Visualisasi data
+        if input_user == 5:
+            pass
+
+        #Statistik data
+        if input_user == 6:
+            pass
 
         #Back to menu
         if input_user == 7:
@@ -84,8 +139,23 @@ def pinjam_buku():
 
         #Read data daftar pinjam
         if input_menu_pinjam == 3:
-            df = read_pinjam()
-            print(df)
+            df_pinjam = read_pinjam()
+            df_total_buku = len(df_pinjam)
+            print(df_pinjam)
+            print(f"Total peminjam {df_total_buku}")
+            print(menu_search_pinjam())
+            while True :
+                input_search_pinjam = int(input("\nMasukkan angka: "))
+                #Search Peminjam
+                if input_search_pinjam == 1:
+                    input_user_pinjam = int(input("\nMasukkan ID peminjam: "))
+                    df_search_pinjam = search_pinjam(input_user_pinjam)
+                    print(df_search_pinjam)
+                elif input_search_pinjam == 2:
+                    # keluar dari menu search → kembali ke menu admin
+                    break
+                else:
+                    print("Pilihan tidak valid!")
 
         #Back to menu
         if input_menu_pinjam == 4:
@@ -112,6 +182,5 @@ def main():
             close_prog()
             run = False 
         
-
 if __name__ == "__main__":
     main()
