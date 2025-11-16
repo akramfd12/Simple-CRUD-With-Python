88390164b10025e7c012b920f33abab1412d2e26
akramfd12 <umarofahrul@gmail.com>
2025-11-16 13:06:20 +0700

    feat: comment out print statements for cleaner output

--- functions/read.py
+++ functions/read.py
@@ -1,7 +1,8 @@
 from .db_config import read_data_book,read_data_pinjam
 import pandas as pd
+from datetime import date, datetime
 def read_book():
-    print("Menampilkan Daftar Buku")
+    # print("Menampilkan Daftar Buku")
     df = pd.DataFrame(read_data_book())
     return df
 
@@ -11,7 +12,7 @@ def search_book(id):
     return df
 
 def read_pinjam():
-    print("\nMenampilkan Daftar Pinjam\n")
+    # print("\nMenampilkan Daftar Pinjam\n")
     df = pd.DataFrame(read_data_pinjam())
     df.rename(columns={'lamahari_pinjam': 'lamahari','flag_pinjam': 'status'}, inplace=True)
     return df
@@ -24,5 +25,12 @@ def search_pinjam(id):
 def merge_book_pinjam():
     df_book = read_book()
     df_pinjam = read_pinjam()
-    df =  pd.merge(df_book, df_pinjam, left_on= "id", right_on= "id_buku")
+    df = pd.merge(df_book, df_pinjam, left_on= "id", right_on= "id_buku")
+    df["tanggal_selesai"] = pd.to_datetime(df["tanggal_selesai"])
+    df["tanggal_pinjam"] = pd.to_datetime(df["tanggal_pinjam"])
+    df["actual_pinjam"] = (df["tanggal_selesai"] - df["tanggal_pinjam"]).dt.days
+    df["actual_pinjam"] = df["actual_pinjam"].fillna(0).astype(int)
+    df["flag_late"] = [True if x > 7 else False for x in df["actual_pinjam"]]
+    df["month_pinjam"] = df["tanggal_pinjam"].dt.month
+    df["year_pinjam"] = df["tanggal_pinjam"].dt.year
     return df
