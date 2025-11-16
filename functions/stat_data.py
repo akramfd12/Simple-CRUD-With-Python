9b44ecc9b2d106dfc30783e5cdfdf31a54791818
akramfd12 <umarofahrul@gmail.com>
2025-11-16 21:55:29 +0700

    feat: remove commented-out code for cleaner statistics functions

--- functions/stat_data.py
+++ functions/stat_data.py
@@ -1,14 +1,6 @@
 import pandas as pd
 from .read import merge_book_pinjam
 
-# def pinjam_statistic_total (id1,id2)):
-#     df = merge_book_pinjam()
-#     df = df[["id_buku","title"]]
-#     df_clean = df.rename(columns={"id_buku": "total"})
-#     count_title = df_clean.groupby(["title"]).count()
-#     # count_title = count_title.sort_values(by=['total'], ascending=False).head(5)
-#     count_title = count_title.describe()
-#     print(count_title)
 
 def pinjam_statistic_total (column_total):
     df = merge_book_pinjam()
@@ -19,7 +11,6 @@ def pinjam_statistic_total (column_total):
     mean_book = count_book_groupby.mean().item()
     median_book = count_book_groupby.median().item()
     unique_orang = df["nama_pinjam"].nunique()
-    # count_title = count_title.describe()
     print(count_book)
     print(f"Mean:{mean_book}")
     print(f"Median:{median_book}")
@@ -35,7 +26,6 @@ def pinjam_statistic_total_year (column_total, year_pinjam):
     mean_book = count_book_groupby.mean().item()
     median_book = count_book_groupby.median().item()
     unique_orang = df_total_filter["nama_pinjam"].nunique()
-    # count_title = count_title.describe()
     print(count_book)
     print(f"Mean:{mean_book}")
     print(f"Median:{median_book}")
@@ -52,7 +42,6 @@ def pinjam_statistic_total_year_month (column_total, year_pinjam, month_start, m
     mean_book = count_book_groupby.mean().item()
     median_book = count_book_groupby.median().item()
     unique_orang = df_total_filter["nama_pinjam"].nunique()
-    # count_title = count_title.describe()
     print(count_book)
     print(f"Mean:{mean_book}")
     print(f"Median:{median_book}")
